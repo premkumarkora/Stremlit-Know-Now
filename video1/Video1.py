96422fcf084d74b25a7b9a5259909265a756c003
PremKumar Kora <49707137+premkumarkora@users.noreply.github.com>
2022-10-27 10:37:23 +0530

    Add files via upload

--- video1/Video1.py
+++ video1/Video1.py
@@ -1,1 +1,34 @@
+# Prerequisites
+# Python IDE
+# Python  Greater than 3.7
+# PIP
 
+
+
+
+# pip install streamlit
+# pip install plotly.express
+# pip install pandas
+# pip install numpy
+# pip install plotly
+
+
+
+
+
+
+import streamlit as st
+import plotly.express as px
+
+st.write("Welcome")
+
+st.markdown('<div style="text-align: center;"><h1>Kora Consultants</H1></div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: center;"><p>Let us leran Stremlit from basics </p>', unsafe_allow_html=True)
+
+line_chart = px.line(
+    x=[1,3,5,7,9],
+    y=[2,4,8,12,16],
+    width=400,
+    height=350
+)
+st.plotly_chart(line_chart)
